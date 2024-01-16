9a9144e7355c23a44366f99417b7f7345849ba0e
SyHarsh2103 <harshpanchal2103@gmail.com>
2024-01-16 12:15:06 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'category',
+    component: SingleCategoryComponent
+  },
+  {
+    path: 'post',
+    component: SinglePostComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  },
+  {
+    path: 'terms-condition',
+    component: TermsAndConditionsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/static/header/header.component';
+import { FooterComponent } from './components/static/footer/footer.component';
+import { CategoryNavbarComponent } from './components/static/category-navbar/category-navbar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SingleCategoryComponent } from './components/pages/single-category/single-category.component';
+import { SinglePostComponent } from './components/pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './components/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
+import { SubscriptionFormComponent } from './components/subscription-form/subscription-form.component';
+import { CommentFormComponent } from './components/comments/comment-form/comment-form.component';
+import { CommentListComponent } from './components/comments/comment-list/comment-list.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryNavbarComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    SubscriptionFormComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    AboutUsComponent,
+    PostCardComponent,
   ],
   imports: [
     BrowserModule,
